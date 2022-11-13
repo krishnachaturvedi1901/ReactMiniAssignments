f0defaba70f47e4338744eea20e6ac632e83d33e
krishnachaturvedi1901 <krishna.chaturvedi1901@gmail.com>
2022-11-13 20:18:08 +0530

    added Bookstore App

--- masai-ops-206-249-rmvTai-b2b96777f9a9ad7941b1327384af6031fae663ee/src/Components/CartButton.jsx
+++ masai-ops-206-249-rmvTai-b2b96777f9a9ad7941b1327384af6031fae663ee/src/Components/CartButton.jsx
@@ -6,11 +6,11 @@ const CartButton = ({itemCount,handleChange}) => {
     <div className = "change_quantity_container"
     style={{width:"100%",fontWeight:"bold",display:"flex",justifyContent:"flex-end"}}
     >
-      <button data-cy = "inc_btn" onClick={()=>handleChange(-1)}
+      <button data-cy = "dec_btn" onClick={()=>handleChange(-1)}
       style={{border:"1px solid #3B3486",borderRadius:"50%",backgroundColor:"#5DA7DB",padding:"8px",cursor:"pointer",fontWeight:"bolder",margin:"8px"}}
       >-</button>
       <p className="quantity">{itemCount}</p>
-      <button data-cy = "dec_btn" onClick={()=>handleChange(1)} 
+      <button data-cy = "inc_btn" onClick={()=>handleChange(1)} 
       style={{border:"1px solid #3B3486",borderRadius:"50%",backgroundColor:"#5DA7DB",padding:"8px",cursor:"pointer",fontWeight:"bolder",margin:"8px"}}
       >+</button>
     </div>
